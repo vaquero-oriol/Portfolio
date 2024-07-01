df86c56e91e29aa45f37c4463dd938e5e06aefca
Oriol <114229876+elxorii@users.noreply.github.com>
2024-07-01 22:16:12 +0200

    Don't knwo what i did really push

--- Calendar/src/main/java/Calendar/Repository/ErrandRepository.java
+++
@@ -1,7 +0,0 @@
-package Calendar.Repository;
-
-import Calendar.Entity.ErrandEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ErrandRepository extends JpaRepository<ErrandEntity,String> {
-}
